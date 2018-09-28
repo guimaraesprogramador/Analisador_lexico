c8ca351905de4f930715e7b768c1a2902e227813
guimaraesprogramador <guimaraesprogramador@gmail.com>
2018-09-28 20:29:49 -0300

    alteracoes ainda

--- Comentario_literal_caracter_separador/Comentario_fonte.cs
+++ Comentario_literal_caracter_separador/Comentario_fonte.cs
@@ -16,43 +16,61 @@ namespace Comentario_literal_caracter_separador
         {
             string[] retorno = new string[5];
             StringBuilder builder = new StringBuilder();
-            for(int i = 0; i < texto.Length; i++)
+            char[] cadeia = texto.ToArray();
+            string conca = "";
+            for(int i = 0; i < cadeia.Length; i++)
             {
-                builder.Append(texto[i]);
-                string a = builder.ToString();
-                switch (a)
+                conca = conca + cadeia[i];
+                if (char.IsLetter(cadeia[i]))
                 {
-                    case "///":
-                        string[] barra_simple = Index.indice(texto, Token.comentarios[3]);
-                        if (barra_simple[0] != "erro") retorno[0] = Token.comentarios[3] + " " + "na posicao " + barra_simple[0] + "linha " + barra_simple[1];
-                        else retorno[0] = "erro lexico";
-                        builder.Clear();
-                        break;
-                    case "/**":
-                        string[] barra = Index.indice(texto, "/**");
-                        if (barra.ToString() != "erro") retorno[1] = Token.comentarios[2] + " " + "na posicao " + barra[0];
-                        else retorno[1] = "erro lexico";
-                        builder.Clear();
-                        break;
-                    case "//":
-                        string[] barra_simple_dupla = Index.indice(texto, "//");
-                        if (barra_simple_dupla[0] != "erro") retorno[2] = Token.comentarios[0].ToString() + " " + " na posicao " + barra_simple_dupla[0] + " linha " + barra_simple_dupla[1];
-                        else retorno[2] = "erro lexico";
-                        builder.Clear();
-                        break;
-                    case "*/":
- 
-                        string[] barra_simple_dupla_final = Index.indice(texto, Token.comentarios[1].ToString());
-                        if (barra_simple_dupla_final[0] != "erro") retorno[3] = Token.comentarios[1].ToString() + " " + " na posicao " + barra_simple_dupla_final[0] + " linha " + barra_simple_dupla_final[1];
-                        else retorno[3] = "erro lexico";
-                        builder.Clear();
-                        break;
-                    case "/r":
-                        string[] remover_quebra_linha = a.Split('\n');
-                        string[] remover_r = remover_quebra_linha[0].Split('\r');
-                        builder.Clear();
-                        break;
+                    i = i + 1;
+                    conca = "";
                 }
+                else
+                {
+                    
+                    switch (conca)
+                    {
+                        case "///":
+                            string[] barra_simple = Index.indice(texto, Token.comentarios[3]);
+                            if (barra_simple[0] != "erro") retorno[0] = Token.comentarios[3] + " " + "na posicao " + barra_simple[0] + "linha " + barra_simple[1];
+                            else retorno[0] = "erro lexico";
+                            conca = "";
+                            continue;
+                        case "/**":
+                            string[] barra = Index.indice(texto, "/**");
+                            if (barra.ToString() != "erro") retorno[1] = Token.comentarios[2] + " " + "na posicao " + barra[0];
+                            else retorno[1] = "erro lexico";
+                            conca = "";
+                            continue;
+                        case "//":
+                            string[] barra_simple_dupla = Index.indice(texto, "//");
+                            if (barra_simple_dupla[0] != "erro") retorno[2] = Token.comentarios[0].ToString() + " " + " na posicao " + barra_simple_dupla[0] + " linha " + barra_simple_dupla[1];
+                            else retorno[2] = "erro lexico";
+                            conca = "";
+                            continue;
+                        case "/*":
+
+                            string[] barra_simple_dupla_final = Index.indice(texto, Token.comentarios[1].ToString());
+                            if (barra_simple_dupla_final[0] != "erro") retorno[3] = Token.comentarios[1].ToString() + " " + " na posicao " + barra_simple_dupla_final[0] + " linha " + barra_simple_dupla_final[1];
+                            else retorno[3] = "erro lexico";
+                            conca = "";
+                            continue;
+                        case "/r":
+                            conca = "";
+                            continue;
+                        case "/n":
+                            conca = "";
+                            continue;
+                        default:
+                            if (i == texto.Length)
+                            {
+                                break;
+                            }
+                            continue;
+                    }
+                }
+
             }
             return retorno;
             
